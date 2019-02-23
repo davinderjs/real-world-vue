8bb3bb4bdb9e4cc48a0faf269db693ab81d33d06
Davinder Singh <davinder.jsingh@gmail.com>
2019-02-23 16:31:57 -0500

    Add Notifications Module

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -95,9 +95,7 @@ export default {
           });
           this.event = this.createFreshEventObject();
         })
-        .catch(() => {
-          console.log("There was a problem creating your event.");
-        });
+        .catch(() => {});
     }
   },
   computed: {
