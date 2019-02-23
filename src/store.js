81761de4b92c6fdd036c631985abefc1285bcb5c
Davinder Singh <davinder.jsingh@gmail.com>
2019-02-22 23:37:59 -0500

    Moved service call to vuex store

--- src/store.js
+++ src/store.js
@@ -19,16 +19,14 @@ export default new Vuex.Store({
       "food",
       "community"
     ],
-    events: [
-      { id: 1, title: "...", organizer: "..." },
-      { id: 2, title: "...", organizer: "..." },
-      { id: 3, title: "...", organizer: "..." },
-      { id: 4, title: "...", organizer: "..." }
-    ]
+    events: []
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event);
+    },
+    SET_EVENTS(state, events) {
+      state.events = events;
     }
   },
   actions: {
@@ -36,6 +34,15 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit("ADD_EVENT", event);
       });
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+        .then(response => {
+          commit("SET_EVENTS", response.data);
+        })
+        .catch(error => {
+          console.log("There was an error: " + error.data);
+        });
     }
   },
   getters: {
