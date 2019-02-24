a8320b9f1d57a4ee8cd18dea74c1179891c9b2fd
Davinder Singh <davinder.jsingh@gmail.com>
2019-02-23 21:44:53 -0500

    Error Handling - Part 1

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -9,33 +9,21 @@
       <h3>Name & describe your event</h3>
       <div class="field">
         <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
+        <input v-model="event.title" type="text" placeholder="Add an event title">
       </div>
       <div class="field">
         <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
+        <input v-model="event.description" type="text" placeholder="Add a description">
       </div>
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
+        <input v-model="event.location" type="text" placeholder="Add a location">
       </div>
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker v-model="event.date" placeholder="Select a date"/>
       </div>
       <div class="field">
         <label>Select a time</label>
@@ -43,7 +31,7 @@
           <option v-for="time in times" :key="time">{{ time }}</option>
         </select>
       </div>
-      <input type="submit" class="button -fill-gradient" value="Submit" />
+      <input type="submit" class="button -fill-gradient" value="Submit">
     </form>
   </div>
 </template>
@@ -51,6 +39,7 @@
 <script>
 import { mapState, mapGetters } from "vuex";
 import Datepicker from "vuejs-datepicker";
+import NProgress from "nprogress";
 
 export default {
   components: {
@@ -86,6 +75,7 @@ export default {
       };
     },
     createEvent() {
+      NProgress.start();
       this.$store
         .dispatch("event/createEvent", this.event)
         .then(() => {
@@ -95,7 +85,9 @@ export default {
           });
           this.event = this.createFreshEventObject();
         })
-        .catch(() => {});
+        .catch(() => {
+          NProgress.done();
+        });
     }
   },
   computed: {
